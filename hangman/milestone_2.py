9bb9b0e6b48b9d02084c0872400c2f429de45ca4
Will Armitage <will@nellacre.com>
2023-10-20 11:02:28 +0100

    Creating the milestone_2 file, up to checking lenth of guess

---
+++ hangman/milestone_2.py
@@ -0,0 +1,22 @@
+#Importing Modules:
+import random
+
+#Creating list of fruits
+word_list = ["Strawberry", "Blueberry", "Mangosteen", "Pineapple", "Apple"]
+print(word_list)
+
+#Selecting Random Fruit
+
+word = random.choice(word_list)
+print(word)
+
+#Taking single letter guess:
+
+
+guess = input("Please enter single letter guess: ")
+if len(guess) == 1:
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input")
+    
+
